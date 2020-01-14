7a041087edc46a3e9bbe68d9d1566baf21dc8acf
Conor Walsh <conorwalsh0703@gmail.com>
2020-01-14 12:54:35 +0000

    created header, inventory & order components

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import Header from "./Header"
+import Order from "./Order"
+import Inventory from "./Inventory"
 
 class App extends React.Component {
     render() {
         return (
+            
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <header />
+                    <Header />
 
                 </div>
-                <Inventory />
-                <Order />
+                <Order></Order>
+                <Inventory></Inventory>
             </div>
+            
         )
     }
 }
