2846c3cc03dc9651c57ae4126eab552ab5b621b0
Conor Walsh <conorwalsh0703@gmail.com>
2020-01-21 12:34:51 +0000

    displaying order state with JSX

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -51,7 +51,7 @@ class App extends React.Component {
               <Fish
                 key={key}
                 index = {key}
-                
+
                 details={this.state.fishes[key]}
                 addToOrder={this.addToOrder}>
                 {key}
@@ -59,7 +59,7 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order></Order>
+        <Order fishes = {this.state.fishes} order = {this.state.order}></Order>
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}></Inventory>
